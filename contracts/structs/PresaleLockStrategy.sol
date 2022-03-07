01e7ca0a2168158319c37b4eecfe8603217d3dde
hotpotnetwork <101038799+hotpotnetwork@users.noreply.github.com>
2022-03-07 14:52:52 +0800

    Create PresaleLockStrategy.sol

---
+++ contracts/structs/PresaleLockStrategy.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+struct PresaleLockStrategy {
+
+    uint256 lockTime;
+
+    uint256 releaseTime;
+
+    uint256 startTime;
+
+    uint256 releaseUnit;
+
+}
